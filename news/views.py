024791856bca37cbbf7a8a273bb622686c91e251
nion8 <nion8@mail.ru>
2018-08-13 09:01:33 +0300

    new_list url

--- news/views.py
+++ news/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from news.models import News
+
 
 # Create your views here.
+
+def news_list(request):
+    """вывод всех новостей"""
+    news = News.objects.all()
+    return render(request, "news/news_list.html", {"news":news})
+
+def new_single(request, pk):
+    """вывод полной статьи"""
+    new = get_object_or_404(News, id=pk)
+    return render(request, "news/news_single.html", {"new": new})
