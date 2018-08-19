eac0a7e3c637092015661d6d70e15667980cd94a
nion8 <nion8@mail.ru>
2018-08-19 19:23:21 +0300

    text redactor

--- dlog_my/settings.py
+++ dlog_my/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- news/admin.py
+++ news/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from news.models import News, Category, Tag
 
+
+class NewsAdmin(SummernoteModelAdmin):
+    summernote_fields = ('text_min', 'text')
+
+
 # Register your models here.
-admin.site.register(News)
+admin.site.register(News, NewsAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
