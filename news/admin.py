104ee6e3f6f5eb2a1157d7cd29c5c3e6362177d8
nion8 <nion8@mail.ru>
2018-08-20 11:31:47 +0300

    moderaciy stati

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from news.models import News, Category, Tag
+from news.models import News, Category, Tag, Comments
 
 
 class NewsAdmin(SummernoteModelAdmin):
@@ -11,3 +11,10 @@ class NewsAdmin(SummernoteModelAdmin):
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'new', 'created', 'moderation')
+
+
+admin.site.register(Comments, CommentAdmin)
